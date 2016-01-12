51b779d1765b9691bd85b1d4869dcdff346aad8f
liamscofield <limitscofield@gmail.com>
2016-01-12 13:41:28 +0800

    服务器状态

---
+++ HomeAccess/ServerInfoDataFrame.swift
@@ -0,0 +1,13 @@
+//
+//  ServerInfoDataFrame.swift
+//  HomeAccess
+//
+//  Created by AlienLi on 16/1/12.
+//  Copyright © 2016年 MarcoLi. All rights reserved.
+//
+
+import UIKit
+
+class ServerInfoDataFrame: NSObject {
+
+}
---
+++ HomeAccess/ServerInfoViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ServerInfoViewModel.swift
+//  HomeAccess
+//
+//  Created by AlienLi on 16/1/12.
+//  Copyright © 2016年 MarcoLi. All rights reserved.
+//
+
+import UIKit
+
+class ServerInfoViewModel: NSObject {
+
+}
