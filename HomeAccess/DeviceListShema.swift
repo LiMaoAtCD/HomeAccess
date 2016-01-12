60ef851708df8bec329006094cc854ea1a6959ef
liamscofield <limitscofield@gmail.com>
2016-01-12 14:36:09 +0800

    设备列表

---
+++ HomeAccess/DeviceListShema.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceListShema.swift
+//  HomeAccess
+//
+//  Created by AlienLi on 16/1/12.
+//  Copyright © 2016年 MarcoLi. All rights reserved.
+//
+
+import UIKit
+
+class DeviceListShema: NSObject {
+
+}
---
+++ HomeAccess/DeviceListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceListViewModel.swift
+//  HomeAccess
+//
+//  Created by AlienLi on 16/1/12.
+//  Copyright © 2016年 MarcoLi. All rights reserved.
+//
+
+import UIKit
+
+class DeviceListViewModel: NSObject {
+
+}
